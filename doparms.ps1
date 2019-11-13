d90cfca04e8ee49b22d2d0a7899a4c3f7fc8d9a2
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2019-11-13 13:07:42 -0500

    First POC

---
+++ doparms.ps1
@@ -0,0 +1,14 @@
+﻿param(
+[string]$environement="dev",
+[string]$client="wj",
+[string]$product="deicingmanager"
+)
+
+cd $PSScriptRoot
+
+$jsondata=Get-Content -Path .\azuredeploy2.json -Raw
+
+$jsondata = ($jsondata -replace '%%product%%', $product)
+$jsondata = ($jsondata -replace '%%clientinit%%', $client)
+$jsondata = ($jsondata -replace '%%env%%', $environement)
+set-content .\azuredeploy.json -Value $jsondata
