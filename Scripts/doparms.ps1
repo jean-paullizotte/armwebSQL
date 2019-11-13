a40ead44b407452d26fe60dcc1acffdc66f4f895
Jean-Paul Lizotte <jean-paul.lizotte@emyode.com>
2019-11-13 13:12:20 -0500

    Delete extraneous fix script

--- Scripts/doparms.ps1
+++ Scripts/doparms.ps1
@@ -6,9 +6,9 @@
 
 cd $PSScriptRoot
 
-$jsondata=Get-Content -Path .\azuredeploy2.json -Raw
+$jsondata=Get-Content -Path .\Templates\azuredeploy2.json -Raw
 
 $jsondata = ($jsondata -replace '%%product%%', $product)
 $jsondata = ($jsondata -replace '%%clientinit%%', $client)
 $jsondata = ($jsondata -replace '%%env%%', $environement)
-set-content .\azuredeploy.json -Value $jsondata
+set-content .\Templates\azuredeploy.json -Value $jsondata
